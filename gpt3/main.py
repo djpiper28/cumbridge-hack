535532d466902f863cdcd16bcb7b2f9169ce0203
Danny Piper <djpiper28@gmail.com>
2023-03-12 12:57:19 +0000

    fix: dont need that file

--- gpt3/main.py
+++ gpt3/main.py
@@ -1,6 +1,5 @@
 import openai
 import os
-import whisper
 from elevenlabslib import *
 
 model = whisper.load_model("base")
@@ -9,30 +8,34 @@ voice = user.get_voices_by_name("AndrewTate2")[0]
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-messages = [{"role": "system", "content": "You are simulating a conversation between a person and an emotional support horse named andrew who also does therapy. You play the role of the horse."}]
+messages = [
+    {
+        "role": "system",
+        "content": "You are simulating a conversation between a person and an emotional support horse named andrew who also does therapy. You play the role of the horse.",
+    }
+]
 
 
 while True:
+    # Get the user's input
+    user_input = input("You: ")
 
-  # Get the user's input
-  user_input = input("You: ")
+    if user_input == "":
+        break
 
-  if user_input == "":
-    break
+    # Add the user's input to the messages
+    messages.append({"role": "user", "content": user_input})
 
-  # Add the user's input to the messages
-  messages.append({"role": "user", "content": user_input})
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        temperature=0.9,
+        max_tokens=150,
+    )
 
-  response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=messages,
-    temperature=0.9,
-    max_tokens=150,
-  )
+    msgResponse = response.choices[0].message.content
 
-  msgResponse = response.choices[0].message.content
+    # Add the bot's response to the messages
+    messages.append({"role": "system", "content": msgResponse})
 
-  # Add the bot's response to the messages
-  messages.append({"role": "system", "content": msgResponse})
-
-  voice.generate_and_play_audio(msgResponse, playInBackground=True)
+    voice.generate_and_play_audio(msgResponse, playInBackground=True)
